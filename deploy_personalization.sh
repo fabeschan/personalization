1a1b468bcf48c6e765763ddd10b08790950759e6
Fabian Chan <fchan@demonware.net>
2015-10-16 15:51:19 -0700

    Init commit

---
+++ deploy_personalization.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+confirm () {
+    # call with a prompt string or use a default
+    read -r -p "${1:-Are you sure? [y/N]} " response
+    case $response in
+        [yY][eE][sS]|[yY]) 
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
+confirm || exit 0
+
+ln -s ./vimrc ~/.vimrc
+ln -s ./tmux.conf ~/.tmux.conf
+
+echo "Done"
---
+++ vimrc
@@ -0,0 +1,7 @@
+set tabstop=4
+set expandtab
+set softtabstop=4
+set shiftwidth=4
+filetype indent on
+syntax on
+filetype indent plugin on
